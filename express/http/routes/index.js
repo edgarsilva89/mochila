a23443f3c3edb49cd42a091d50c450805107aeb6
edgarsilva89 <edgarsilva89@gmail.com>
2021-11-10 20:48:27 -0300

    atualizando git com a aula

--- express/http/routes/index.js
+++ express/http/routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let v1 = 10;
-  let v2 = 20
-  const r = v1*v2;
-  
-  res.render('index');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
